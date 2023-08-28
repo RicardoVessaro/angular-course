131ad8792bf3d1342880f94055e5ad0e6313e416
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-28 19:44:24 -0300

    34. #490 Array & Object Types

--- section-34/basics.ts
+++ section-34/basics.ts
@@ -11,3 +11,27 @@ let userName: string;
 userName = 'Max';
 
 let isIntstructor: boolean = true;
+
+// More complex types
+
+let hobbies: string[] = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+}; 
+
+person = {
+    name:'Max',
+    age: 32
+}
+
+// Compile error because 'isEmployee' is not defined.
+// person = {
+//     isEmployee: true
+// }
+
+let people: {
+    name: string;
+    age: number;
+}[]; // Array of objects 
