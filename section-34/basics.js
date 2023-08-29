d5014d500b814263706fc90f5a085f42184a368f
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-28 21:19:00 -0300

    34. #498 Configuring the TypeScript Compiler

--- section-34/basics.js
+++ section-34/basics.js
@@ -1,23 +1,15 @@
+"use strict";
 // Primitives: number, string, boolean
 // More complex types: arrays, objects
 // Function types, parameters
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 // Primitives
-var age = 24;
-var userName;
+let age = 24;
+let userName;
 userName = 'Max';
-var isIntstructor = true;
+let isIntstructor = true;
 // More complex types
-var hobbies = ['Sports', 'Cooking'];
-var person;
+let hobbies = ['Sports', 'Cooking'];
+let person;
 person = {
     name: 'Max',
     age: 32
@@ -26,9 +18,9 @@ person = {
 // person = {
 //     isEmployee: true
 // }
-var people; // Array of objects
+let people; // Array of objects
 // Type inference
-var course = 'React - The Complete Guide';
+let course = 'React - The Complete Guide';
 course = 1234;
 // Functions & types
 function add(a, b) {
@@ -40,15 +32,15 @@ function say(value) {
 }
 // Generics
 function insertAtBeginning(array, value) {
-    var newArray = __spreadArray([value], array, true);
+    const newArray = [value, ...array];
     return newArray;
 }
-var demoArray = [1, 2, 3];
-var updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
-var stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
 // updatedArray[0].split(''); error, infered type number.
 // Classes
-var Student = /** @class */ (function () {
+class Student {
     // Declaring attributes ahead of time;
     // firstName: string;
     // lastName: string;
@@ -61,18 +53,17 @@ var Student = /** @class */ (function () {
     //      this.courses = courses;
     // }
     // Short way
-    function Student(firstName, lastName, age, courses) {
+    constructor(firstName, lastName, age, courses) {
         this.courses = courses;
     }
-    Student.prototype.enrol = function (courseName) {
+    enrol(courseName) {
         this.courses.push(courseName);
-    };
-    Student.prototype.listCourses = function () {
+    }
+    listCourses() {
         return this.courses.slice();
-    };
-    return Student;
-}());
-var student = new Student('Max', 'Schwarz', 32, ['Angular']);
+    }
+}
+const student = new Student('Max', 'Schwarz', 32, ['Angular']);
 student.enrol('React');
 student.listCourses();
 // student.courses => Angular, React
@@ -80,14 +71,25 @@ student.listCourses();
  Classes - Test
     Trying to dneclare attributes ahead of time and with short constructor.
 */
-var StudentTest = /** @class */ (function () {
-    function StudentTest(name) {
+class StudentTest {
+    constructor(name) {
         this.name = name;
     }
-    StudentTest.prototype.getAge = function () { return this.age; };
-    StudentTest.prototype.setAge = function (age) { this.age = age; };
-    return StudentTest;
-}());
-var studentTest = new StudentTest('John');
+    getAge() { return this.age; }
+    setAge(age) { this.age = age; }
+}
+const studentTest = new StudentTest('John');
 studentTest.setAge(30);
 console.log(studentTest.name, " - ", studentTest.getAge());
+let max = {
+    firstName: 'Max',
+    age: 32,
+    greet() {
+        console.log('Hello!');
+    },
+};
+class Instructor {
+    greet() {
+        console.log('Hello!!!!');
+    }
+}
