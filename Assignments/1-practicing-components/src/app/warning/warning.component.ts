a9ca92679ba034d5ce5489aa12506a52eb4c221b
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-02 12:25:25 -0300

    A1 Exercise changes already commited app component

---
+++ Assignments/1-practicing-components/src/app/warning/warning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning',
+    template: '<p>This is a warning message.</p>',
+    styles: [` p { color: orange }`]
+})
+export class WarningComponent {
+
+}
