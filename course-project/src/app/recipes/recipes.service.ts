96dc961e9382b9ed75b84f1b6a51e94428928bc6
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-19 20:19:11 -0300

    S10#122 Adding Ingredients to Recipes

--- course-project/src/app/recipes/recipes.service.ts
+++ course-project/src/app/recipes/recipes.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+        new Recipe(
+            'Tasty Schnitzel', 
+            'A super-tasty Schnitzel - just awesome!', 
+            'https://www.cozinhatecnica.com/wp-content/uploads/2020/08/receita-de-schnitzel.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Big Fat Burger', 
+            'What else need to say?', 
+            'https://wessel.com.br/cdn/shop/products/WESSEL_Burger_picanha.jpg?v=1678382207', 
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('meat', 1)
+            ])
     ];
 
     getRecipes() {
