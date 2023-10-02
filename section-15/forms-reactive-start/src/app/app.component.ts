5cf3219c2fbad9b62c962b734dcccfde2b845164
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-01 23:06:30 -0300

    S15#216 R: Setting and Patching Values

--- section-15/forms-reactive-start/src/app/app.component.ts
+++ section-15/forms-reactive-start/src/app/app.component.ts
@@ -29,10 +29,26 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (value) => console.log(value)
     );
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset({gender: 'female'});
   }
 
   onAddHobby() {
