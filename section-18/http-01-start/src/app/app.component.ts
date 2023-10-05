44572ee678a630b2d4a3d3e173967ce95214340e
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-04 21:59:42 -0300

    S18 #259 Using Types with the HttpClient

--- section-18/http-01-start/src/app/app.component.ts
+++ section-18/http-01-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +17,11 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     
     // .json is a firebase requirement
-    this.http.post(
+    this.http.post<{ name: string }>(
       'https://ng-complete-guide-1789c-default-rtdb.firebaseio.com/posts.json', 
       postData
     ).subscribe(responseData => {
@@ -39,9 +40,9 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get('https://ng-complete-guide-1789c-default-rtdb.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray = [];
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-1789c-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postsArray.push({ ...responseData[key], id: key});
