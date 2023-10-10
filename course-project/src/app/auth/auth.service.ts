d7e3d6a6e2f7f118643668c494d65c62d85fbfcb
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-09 21:03:59 -0300

    S20 #298 Sending Login Request

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -38,5 +39,16 @@ export class AuthService {
 
             return throwError(() => new Error(errorMessage));
         }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-qZcCXDfck9WyVtu4YAQwRLodZyShEQE',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
     }
 }
