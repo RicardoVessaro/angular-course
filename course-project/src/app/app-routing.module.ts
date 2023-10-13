84e7a78d7f7fafbbff259ecedd8ab3556ad48185
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-13 19:58:45 -0300

    S20 #307 Adding an Auth Guard

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -7,14 +7,17 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipesStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent, resolve: {recipes: RecipeResolverService.fetchRecipes} },
-        { path: ':id/edit', component: RecipeEditComponent, resolve: {recipes: RecipeResolverService.fetchRecipes} }
+    { path: 'recipes', component: RecipesComponent, 
+        canActivate: [AuthGuard],
+        children: [
+            { path: '', component: RecipesStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent, resolve: {recipes: RecipeResolverService.fetchRecipes} },
+            { path: ':id/edit', component: RecipeEditComponent, resolve: {recipes: RecipeResolverService.fetchRecipes} }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'auth', component: AuthComponent }
