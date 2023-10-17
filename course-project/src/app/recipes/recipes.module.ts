a90165458a53a73c7f1cb6c1344b2bbfda281bd4
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-16 21:36:49 -0300

    S22 #329 Understanding Shared Modules

--- course-project/src/app/recipes/recipes.module.ts
+++ course-project/src/app/recipes/recipes.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipesStartComponent } from "./recipes-start/recipes-start.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipesComponent } from "./recipes.component";
-import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -20,10 +21,10 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
         RecipeEditComponent
     ],
     imports: [
-        RouterModule, 
-        CommonModule,
+        RouterModule,
         ReactiveFormsModule,
-        RecipesRoutingModule
+        RecipesRoutingModule,
+        SharedModule
     ]
 })
 export class RecipesModule {
