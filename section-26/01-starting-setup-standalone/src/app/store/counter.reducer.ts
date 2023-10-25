b9e072d76cf9d0455ac7f5670e6440d9641de677
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-24 21:16:30 -0300

    S26 #371 An Alternative Way of Creating Reducers

--- section-26/01-starting-setup-standalone/src/app/store/counter.reducer.ts
+++ section-26/01-starting-setup-standalone/src/app/store/counter.reducer.ts
@@ -2,7 +2,11 @@ import { createReducer } from "@ngrx/store";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState
-);
+// export const counterReducer = createReducer(
+//     initialState
+// );
 
+// Alternative/Older way
+export function counterReducer(state = initialState) {
+    return state;
+}
