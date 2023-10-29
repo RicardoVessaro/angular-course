7854fc9331196b3a0f196ece8c2cde23fcfb190f
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-28 22:52:14 -0300

    S26 #381 Installing the Effects Package

--- section-26/01-starting-setup-standalone/src/main.ts
+++ section-26/01-starting-setup-standalone/src/main.ts
@@ -3,9 +3,10 @@ import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
     providers: [provideStore({
         counter: counterReducer
-    })]
+    }), provideEffects()]
 });
