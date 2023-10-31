bed148941c7150d7a0437d6a293e7f39dc699cc3
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-31 19:54:34 -0300

    S26 #383 The Old @Effect Decorator & Registering Effects

--- section-26/01-starting-setup-standalone/src/app/store/counter.effects.ts
+++ section-26/01-starting-setup-standalone/src/app/store/counter.effects.ts
@@ -1,7 +1,9 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
 import { tap } from "rxjs";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CounterEffects {
     
     saveCount = createEffect(
@@ -16,7 +18,16 @@ export class CounterEffects {
         { dispatch: false }
     );
 
-    
+    // Old way no longer supported
+    // import { Effect } from "@ngrx/effects";
+    // @Effect({ dispatch: false }) 
+    // saveCount = this.actions$.pipe(
+    //         ofType(increment, decrement),
+    //         tap((action) => {
+    //             console.log(action);
+    //             localStorage.setItem('count', action.value.toString());
+    //         })
+    //     );
 
     constructor(private actions$: Actions) {}
 }
