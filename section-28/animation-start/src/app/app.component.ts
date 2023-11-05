8ab23ad73a1a6b9678d030d57ceeaa84ee1c26e3
RicardoVessaro <ricardolvessaro@gmail.com>
2023-11-05 20:18:05 -0300

    S28 #451 Using Animation CAllbacks

--- section-28/animation-start/src/app/app.component.ts
+++ section-28/animation-start/src/app/app.component.ts
@@ -128,4 +128,12 @@ export class AppComponent {
       }
       this.list = updatedList;
     }
+
+    animationStarted(event) {
+      console.log(event);
+    }
+
+    animationEnded(event) {
+      console.log(event);
+    }
 }
