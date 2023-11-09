2ecba1f3931a938bd1359a870279ebc3390ab53f
RicardoVessaro <ricardolvessaro@gmail.com>
2023-11-08 21:04:33 -0300

    S32 #485 A First Look At Angular Elements

--- section-32/angular-elements/src/app/app.component.ts
+++ section-32/angular-elements/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AlertComponent } from './alert.component';
+import { createCustomElement } from '@angular/elements';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AlertComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-elements';
+  content :any = null;
+
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {
+    const AlertElement = createCustomElement(AlertComponent, {injector: injector});
+    customElements.define('my-alert', AlertElement);
+
+    setTimeout(() => {
+      this.content = domSanitizer.bypassSecurityTrustHtml(
+        "<my-alert message='Rendered dynamically'></my-alert>"
+      );
+    }, 1000);
+  }
 }
